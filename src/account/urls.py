4575e8a09beaac7a2a44da875a7681bee853f0c5
Amanovth <mirlanuchyngyz@gmail.com>
2023-08-27 15:10:12 +0600

    add resfresh token endpoint

--- src/account/urls.py
+++ src/account/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 from rest_framework.routers import DefaultRouter
 
 from .views import RegisterView, LoginView
+from rest_framework_simplejwt.views import token_refresh
 
 # router = DefaultRouter()
 # router.register(r'register', RegisterViewSet, basename='register')
@@ -11,5 +12,6 @@ from .views import RegisterView, LoginView
 
 urlpatterns = [
     path('register/', RegisterView.as_view(), name='register'),
-    path('login/', LoginView.as_view(), name='login')
+    path('login/', LoginView.as_view(), name='login'),
+    path('token_refresh/', token_refresh, name='token_refresh')
 ]
